69b1c98eb3ad896e08a7f5c7a7edbca9dbc4c9e7
Marcos Manzanares <marcosmanzanarescampo@gmail.com>
2025-04-03 16:28:18 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 import express from "express";
+import { connecter } from "./bd/connect.js";
 
 const app = express();
-
 
 app.get('/', (req, res) => {
     console.log('Breakpoin processed.');    
 })
 
-
-app.listen(3000);
-console.log('Server mongo is working on! http://localhost/3000');
+connecter("mongodb://127.0.0.1:27017/", (error) => {
+    if(error) {
+        console.log("Erreur lors de la connexion à la base de données");
+        process.exit(-1);     
+    }else {
+        console.log("Connexion avec la basse de données établie");
+        app.listen(3000);
+        console.log('Server mongo is working on! http://localhost/3000');        
+    }
+});
